abb15a667d3e8380353cc6573673b316b663b93d
satyajit <satyajeetsahu53@gmail.com>
2025-12-08 21:11:45 +0530

    Adding project

---
+++ generate.py
@@ -0,0 +1,34 @@
+import os
+import time,subprocess
+from text_to_speech import text_to_speech_file
+
+def text_to_audio(folder) :
+    print("TTA:",folder)
+    with open(f"user_uploads/{folder}/desc.txt") as f:
+        text = f.read()
+    print(text, folder)
+    text_to_speech_file(text,folder)
+
+def create_reel(folder):
+    command = '''ffmpeg -f concat -safe 0 -i user_uploads/{folder}/input.txt -i user_uploads/{folder}/output_reel_with_audio.mp3 -vf
+                            "scale=1080:1920:force_original_aspect_ratio=decrease,pad=1080:1920:(ow-iw)/2:(oh-ih)/2:black" -c:v libx264 -c:a aac -shortest -r 30 -pix_fmt yuv420p static/reels/{folder}.mp4
+                '''
+    subprocess.run(command, shell=True, check=True)
+    print("CR:",folder)
+
+if __name__ == "__main__" :
+    while True:
+        print("Processing queue...")
+        with open("done.txt","r") as f:
+            done_folders = f.readlines()
+        done_folders = [f.strip() for f in done_folders]
+        folders = os.listdir("user_uploads")
+        for  folder in folders :
+            if folder not in done_folders:
+                text_to_audio(folder)
+                create_reel(folder)
+
+            with open("done.txt","a") as f:
+                f.write(folder + "\n")
+
+        time.sleep(4)
---
+++ main.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template,request
+import uuid
+import os
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER ='user_uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/create", methods=["GET","POST"])
+def create():
+    my_id = uuid.uuid1()
+    input_files = []    
+    if request.method   == "POST" :
+        print(request.files.keys())   # isse upload ke data mil jayange
+        rec_id = request.form.get("uuid")  # isse ak unique id mil jayega
+        desc = request.form.get("text")    #jo humlog text area me likhenge yaha pe show hoga
+        for key,value in request.files.items():
+            print(key,value)
+            #upload the file
+            file = request.files[key]
+            if file:
+                filename = secure_filename(file.filename)
+                if(not(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], rec_id)))):
+                    os.mkdir(os.path.join(app.config['UPLOAD_FOLDER'], rec_id))
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'],rec_id, filename))
+            #capture the description and save it to a file
+                input_files.append(file.filename)
+                print(file.filename)
+        with open(os.path.join(app.config['UPLOAD_FOLDER'], rec_id, "desc.txt"),"w") as f:
+            f.write(desc)
+    for fl in input_files:
+        with open(os.path.join(app.config['UPLOAD_FOLDER'], rec_id,"input.txt"),"a") as f:    # file 'user_uploads/d658c606-d2c5-11f0-a04c-fe57aa71bd8e/Screenshot 2025-11-29 110536.png'duration 1 aise store hoga  
+            f.write(f"file '{fl}'\nduration 1\n")
+
+    return render_template("create.html",my_id = my_id)
+
+@app.route("/gallery")
+def gallery():
+    reels = os.listdir("static/reels")
+    return render_template("gallery.html",reels = reels)
+
+app.run(debug=True)
